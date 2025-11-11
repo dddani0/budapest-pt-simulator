b8166574217ee01278a46b0c241ff7fb69ae4b75
Devley0 <getkhun@gmail.com>
2025-11-11 11:11:53 +0100

    init

---
+++ budapest-pt-sim-angular/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar';
+import { Header } from './header/header';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Navbar, Header],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+
+}
